a63da29be44af06b13e7aa0e9af74f2fb5310dae
Mandy Huang <mandyxhuang@gmail.com>
2014-11-29 22:58:11 -0800

    configured controllers, initiated view basics

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,40 @@ class CommentsController < ApplicationController
   def show
   end
 
+  def new
+  end
+
   def update
+    @comment = Comment.find(params[:id])
+    @comment.update_attributes(comment_params)
+    render events_show_path(@comment.event_id)
   end
 
   def edit
+    @comment = Comment.find(params[:id])
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.update_attributes(:user_id => current_user.id)
+    saved = @comment.save
+    if saved
+      redirect_to events_show_path(@comment.event_id)
+    else
+      flash[:error] = @comment.errors.full_messages.to_sentence
+      render comments_new_path
+    end
   end
+
+  def delete
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to events_show_path(@comment.event_id)
+  end
+
+  def comment_params
+    params.require(:event).permit(:attending)
+  end
+
 end
+
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,41 @@
 class EventsController < ApplicationController
   def index
+    #displays all the events
+    @events = Event.all
   end
 
+  def new
+  end
+  
   def show
+    #displays the event page
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
+    @event.update_attributes(event_params)
+    render events_show_path(@event)
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.update_attributes(:user_id => current_user.id)
+    saved = @event.save
+    if saved
+      redirect_to events_show_path(@event)
+    else
+      flash[:error] = @event.errors.full_messages.to_sentence
+      render events_new_path
+    end
   end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :time)
+  end
+
 end
