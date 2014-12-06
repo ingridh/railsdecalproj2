727edae82c078c26f51e070d72151e2bdc125d94
Ingrid <ingridhong@gmail.com>
2014-12-06 02:47:46 -0800

    commented out

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
 	belongs_to :event
 	belongs_to :user
 	validates :attending, presence => true
-	validates :user_id, presence => true, uniqueness => true
-	validates :event_id, presence => true, uniqueness => true
+	validates :user_id, presence => true
+	validates :event_id, presence => true
 end
